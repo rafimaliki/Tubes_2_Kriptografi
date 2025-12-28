0d3a58a251895743305c786e07dc2d9d7501903c
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2025-12-28 18:23:49 +0700

    delete: user schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,29 +1,23 @@
 import { pgTable, serial, text, timestamp, jsonb } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  email: text("email").notNull().unique(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
-
 export const ledger = pgTable("ledger", {
   id: serial("id").primaryKey(),
-  
+
   // immutable logic
-  previous_hash: text("previous_hash").notNull(), 
-  current_hash: text("current_hash").notNull(), 
-  
+  previous_hash: text("previous_hash").notNull(),
+  current_hash: text("current_hash").notNull(),
+
   // Type: 'ISSUE' or 'REVOKE'
-  transaction_type: text("transaction_type").notNull(), 
-  
+  transaction_type: text("transaction_type").notNull(),
+
   // Metadata: Stores Diploma Info or Revocation Reason
-  metadata: jsonb("metadata").notNull(), 
-  
+  metadata: jsonb("metadata").notNull(),
+
   // Digital signature of the Issuer (ECDSA) for authenticity
   signature: text("signature").notNull(),
 
   // Access URL for certificate in website
   access_url: text("access_url").notNull(),
-  
+
   created_at: timestamp("created_at").defaultNow(),
 });
